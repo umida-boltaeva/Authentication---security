d7efa137f93a9eb602a086a5d5701d8799e5c7dc
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-03-24 16:06:18 +1100

    install mongoose and connect mongodb

--- app.js
+++ app.js
@@ -2,15 +2,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
-
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
 
 
 
