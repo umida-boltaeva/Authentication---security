49f87a82cc166755b34592d293d2cd1482b4aaea
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-16 15:35:12 +1000

    cookies are perfectly working! yum-yum))

--- app.js
+++ app.js
@@ -15,28 +15,63 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.post("/register", function(req, res){
-    
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        res.redirect("/");        
+      }else{
+        passport.authenticate("local")(req, res, function(){
+          res.redirect("/secrets");
+        })
+      }
+    })
     
 });
 
 app.post("/login", function(req, res){
-
-    
+const user = new User({
+  username: req.body.username,
+  password: req. body.password
 });
 
-
-
+req.login(user, function(err){
+  if(err){
+    console.log(err);    
+  }else {
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secrets");
+    })
+  }
+});
+    
+});
 
 app.get("/", function(req, res){
     res.render("home");
@@ -48,6 +83,14 @@ app.get("/login", function(req, res){
 
 app.get("/register", function(req, res){
     res.render("register");
+});
+
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 
