5b7c470d9047b488b980ede3083a2eb85344715f
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-16 14:09:04 +1000

    cookies and session

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.use(express.static("public"));
@@ -25,43 +26,13 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 app.post("/register", function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
     
-        newUser.save(function(err){
-            if(!err){
-                res.render("secrets")
-            }else{
-                console.log(err);
-                
-            }
-        });
-    });
-
+    
 });
 
 app.post("/login", function(req, res){
 
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
-        if(err){
-            console.log(err);           
-        }else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result === true){
-                        res.render("secrets");
-                    }
-                });
-                    
-                }
-            }
-    });
+    
 });
 
 
